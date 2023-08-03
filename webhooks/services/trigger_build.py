b06fa64b3f4b7c8398d0313ae7a7e68e9c8fe767
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2023-08-03 16:53:03 +0200

    Rely on $PATH for qrexec-client-vm
    This allows providing wrapper to run it outside of qubes too.

--- webhooks/services/trigger_build.py
+++ webhooks/services/trigger_build.py
@@ -37,7 +37,7 @@ class Service:
 
     def qrexec(self, vm, service, input_data=None):
         with subprocess.Popen(
-            ["/usr/bin/qrexec-client-vm", "--", vm, service],
+            ["qrexec-client-vm", "--", vm, service],
             stdin=subprocess.PIPE,
             stdout=subprocess.DEVNULL,
         ) as p:
